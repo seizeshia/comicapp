41211adf6d57b0380e68036c1da0ac4210e451b0
Caesar <caesarshia@gmail.com>
2017-09-12 00:51:08 -0700

    second commit

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose')
 var fs = require('fs')
 var path = require('path')
-mongoose.connect('mongodb://localhost/comicapp');
+mongoose.connect('mongodb://localhost/theappcomic');
 mongoose.Promise = global.Promise;
 var models_path = path.join(__dirname, './../models')
 fs.readdirSync(models_path).forEach(function(file){
