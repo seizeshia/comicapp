6888da640e5a5c8d0071d969b78cee474ff64849
Caesar <caesarshia@gmail.com>
2017-09-12 16:12:16 -0700

    bugfix

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -77,25 +77,26 @@ var salt = bcrypt.genSaltSync(10);
           Users.findOne({username: request.body.username},function(err,user){
             if(err){
               response.json({message: "Error", error:err})
-            }
-            if(user == null){
-              console.log("register request body", request.body)
-              var hash = bcrypt.hashSync(request.body.password,salt);
-              var newuser = new Users();
-              newuser.username = request.body.username;
-              newuser.password =  hash;
-              newuser.email = request.body.email;
-              newuser.location = request.body.location;
-              newuser.save(function(err,saveduser){
-                if(err){
-                  response.sendStatus(500);
-                }else{
-                  request.session.user = saveduser
-                  response.json(newuser)
-                }
-              })
             }else{
-              response.json({message:"Username has been used! please try another or sign in"})
+              if(user == null){
+                console.log("register request body", request.body)
+                var hash = bcrypt.hashSync(request.body.password,salt);
+                var newuser = new Users();
+                newuser.username = request.body.username;
+                newuser.password =  hash;
+                newuser.email = request.body.email;
+                newuser.location = request.body.location;
+                newuser.save(function(err,saveduser){
+                  if(err){
+                    response.sendStatus(500);
+                  }else{
+                    request.session.user = saveduser
+                    response.json(newuser)
+                  }
+                })
+              }else{
+                response.json({message:"Username has been used! please try another or sign in"})
+              }
             }
           })
          }
